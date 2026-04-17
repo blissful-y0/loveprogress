d8cce5bb24638081160832b5675bd1a1a02bf7ae
Bliss Yun <blissful.y0@gmail.com>
2026-04-17 17:56:40 +0900

    chore: 고정배너 리셋 마이그레이션 중립화
    기존 DB의 어드민 설정 이미지/링크 보존을 위해 DELETE/INSERT 로직 제거.
    링크 URL 교체는 130000(도메인 교체), 140000(상대경로 정규화) 마이그레이션이
    담당하고 이미지나 슬롯 구성 변경은 어드민 UI에서 직접 한다.

--- supabase/migrations/20260417120000_reset_fixed_banners.sql
+++ supabase/migrations/20260417120000_reset_fixed_banners.sql
@@ -1,24 +1,6 @@
--- 고정배너(fixed_banner) 슬롯 강제 리셋.
--- 기존 fixed_banner row에 잘못된 이미지가 들어가 있는 경우가 있어
--- 6개 슬롯을 정확한 sort_order/이미지/링크로 재세팅한다.
--- 이후 어드민에서 링크/이미지 수정 가능.
+-- (중립화됨) 원래 fixed_banner 6개 슬롯을 DELETE/INSERT로 리셋하려 했으나,
+-- 기존 DB에 어드민이 설정한 이미지/링크가 있는 상태를 보존하기 위해 no-op 처리.
+-- 링크 URL 교체는 20260417130000, 20260417140000 마이그레이션이 담당한다.
+-- 이미지/슬롯 추가는 어드민 UI에서 직접 관리.
 
-DELETE FROM main_banners WHERE group_type = 'fixed_banner';
-
-INSERT INTO main_banners (group_type, image_key, link_url, sort_order, is_active) VALUES
-  ('fixed_banner', '/img/main/topcarousel/academic-calendar.png',
-   '/info/notices/4a73b5b4-feb4-48b9-870e-9adbb2cd759b',
-   0, true),
-  ('fixed_banner', '/img/main/topcarousel/freshmen-guide.png',
-   '/info/notices/7adfe9f6-cb0a-4f80-8d6c-762dac73e6d8',
-   1, true),
-  ('fixed_banner', '/img/main/topcarousel/arts-education.png',
-   'https://phainaxa.com/game/',
-   2, true),
-  ('fixed_banner', '/img/main/topcarousel/graduation.png',
-   'https://phnxgame.com/',
-   3, true),
-  ('fixed_banner', '/img/main/topcarousel/graduate-program.png',
-   NULL, 4, true),
-  ('fixed_banner', '/img/main/topcarousel/industry-collab.png',
-   NULL, 5, true);
+SELECT 1;
